7551371acdf77b692fede26677533fdd573fb9d6
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-01-19 11:29:49 -0800

    date selector works

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import codecs
 
 import terrain.config as c
 from terrain.topo import Topography
@@ -14,15 +15,19 @@ canvas_color = '#121212'
 axis_color = react.theme.bokeh_theme._json['attrs']['Axis']['axis_label_text_color']
 
 #---User controls start here--#
-resolution = pn.widgets.IntSlider(name="Resolution", start=10, end=300, value=30)
-sigma = pn.widgets.FloatSlider(name="Sigma", start=0.0, end=3.0, value=2) 
 
-@pn.depends(resolution=resolution, sigma=sigma)
-def grids(resolution, sigma, date_index=-6, view_fn=plot_grids):
+date = pn.widgets.DiscreteSlider(name="Date", options=c.DATE_DICT, value=68)
+
+resolution = pn.widgets.IntSlider(name="Resolution", start=10, end=300, value=150)
+sigma = pn.widgets.FloatSlider(name="Sigma", start=0.0, end=3.0, value=1) 
+
+#---plot function--#
+@pn.depends(resolution=resolution, sigma=sigma, date=date)
+def grids(resolution, sigma, date=date, view_fn=plot_grids):
     '''
 
     '''
-    filename = c.TOPO_LIST[date_index]
+    filename = c.TOPO_LIST[date]
     params = {'R': resolution, 'S':sigma}
 
     test = Topography(filename)
@@ -30,55 +35,16 @@ def grids(resolution, sigma, date_index=-6, view_fn=plot_grids):
     return view_fn(fn=filename, grids=grids, bounds=c.BOUNDS, params=params,
                     canvas_color=canvas_color, axis_color=axis_color)
 
-# append elements to sidebar
+# append elements to template
+react.sidebar.append(date)
 react.sidebar.append(resolution)
 react.sidebar.append(sigma)
 
-# Unlike other templates the `ReactTemplate.main` area acts like a GridSpec 
 react.main[:4, :6] = grids
 
-# append header
-header = pn.pane.HTML(
-    """
-    <head>
-        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/hack-font@3/build/web/hack-subset.css">
-        <style>
-            h4 {
-                font-family: Hack, monospace;
-                display: inline;
-            }
-            a.txt:link,
-            a.txt:visited {
-                border-bottom: 1px dashed;
-                color: palevioletred;
-                font-style: normal;
-                font-weight: 400;
-                text-decoration: none;
-            }
-            a.txt:hover,
-            a.txt:focus {
-                border-bottom-style: solid;
-                color: khaki;
-                outline: 1px solid;
-                outline-offset: .125em;
-            }
-        </style>
-    </head>
-    <h4>
-        Terrain-Corrector, by 
-        <a class="txt" href="http://twitter.com/" target="_blank" >Charles Stern</a>
-    </h4>
-    <a href="https://twitter.com" target="_blank">
-        <img alt="User Guide" src="https://img.shields.io/static/v1?style=plastic&label=User&message=Guide&color=blueviolet">
-    </a>
-    <a href="https://pradyunsg.me/furo/customisation/" target="_blank">
-        <img alt="Blog Posts" src="https://img.shields.io/static/v1?style=plastic&label=Blog&message=Posts&color=green">
-    </a>
-    <a href="https://github.com/cisaacstern/model-gui" target="_blank">
-        <img alt="Github Link" src="https://img.shields.io/static/v1?style=plastic&logo=github&label=View&message=Source&color=yellow">
-    </a>
-    """
-)
+html_head = codecs.open("terrain-corrector/static/header.html", 'r')
+header = pn.pane.HTML(html_head.read())
 react.header.append(header)
 
+# define servable
 servable = react.servable()
