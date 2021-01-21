9c0d040f546437b19bbf6157a80d2dee84b5677c
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-01-20 17:41:18 -0800

    all but horz working in _app.py

--- _app.py
+++ _app.py
@@ -1,3 +1,6 @@
+'''
+
+'''
 import codecs
 
 import param
@@ -6,7 +9,8 @@ from panel.template.theme import DarkTheme
 
 import terrain.config as c
 from terrain.topo import Topography
-from terrain.plot import plot_grids
+from terrain.time import TimeSeries, Correction
+from terrain.plot import plot_grids, generate_titles, plot_sun
 
 pn.extension()
 pn.config.sizing_mode = 'scale_both'
@@ -15,70 +19,89 @@ react = pn.template.ReactTemplate(title='', theme=DarkTheme)
 canvas_color = '#121212'
 axis_color = react.theme.bokeh_theme._json['attrs']['Axis']['axis_label_text_color']
 
-#---User controls start here--#
-
-date = pn.widgets.DiscreteSlider(name="Date", options=c.DATE_DICT, value=68)
-
-resolution = pn.widgets.IntSlider(name="Resolution", start=10, end=300, value=150)
-sigma = pn.widgets.FloatSlider(name="Sigma", start=0.0, end=3.0, value=1) 
-
-#---plot function--#
-@pn.depends(resolution=resolution, sigma=sigma, date=date)
-def grids(resolution, sigma, date=date, view_fn=plot_grids):
-    '''
-
-    '''
-    filename = c.TOPO_LIST[date]
-    params = {'R': resolution, 'S':sigma}
-
-    test = Topography(filename)
-    grids = test.return_grids(params['R'], params['S'])
-    return view_fn(fn=filename, grids=grids, bounds=c.BOUNDS, params=params,
-                    canvas_color=canvas_color, axis_color=axis_color)
-
-#--- New work below here 
-
 class Interact(param.Parameterized):
-
-    nums = list(c.DATE_DICT.values())
-    print(nums)
+    '''
+    Brings everything together.
+    '''
+    nums = list(c.DATE_DICT.values()) #update this
     date = param.Selector(default=70, objects=nums)
     resolution = param.Integer(150, bounds=(10, 300))
     sigma = param.Number(1.0, bounds=(0.0, 3.0))
+    bins = param.Integer(32, bounds=(8,64), step=8)
 
-    @param.depends('date', 'resolution', 'sigma')
-    def grids(self, view_fn=plot_grids):
+    def set_view_attributes(self):
+        '''
+        Instance method for resetting key attributes.
+        Note that though this is called from within self.view_grids(),
+        it deliberately sets instance variables.
+        '''
+        self.fname = c.TOPO_LIST[self.date]
+        self.params = {'R': self.resolution, 'S': self.sigma}
+
+        test = Topography(self.fname)
+        self.grids = test.return_grids(self.params['R'], self.params['S'])
+
+    @param.depends('date', 'resolution', 'sigma')#, 'time')
+    def view_terrain(self, view_fn=plot_grids):
+        '''
+        Two key functions here: (a) sets view attributes; and (b) serves
+        the grid plot. dims are variablized because that depends.
+        '''
+        self.set_view_attributes()
+        titles = generate_titles(fn=self.fname, params=self.params, _type='topo')
+        cmaps = ['viridis', 'YlOrBr', 'hsv']
+        dims = {'figsize':(12,5), 'dpi':80, 'wspace':0.05, 'hspace':0, 
+                'left':0.05, 'right':0.97, 'top':0.79, 'bottom':0.1}
+
+        return view_fn(grids=self.grids, params=self.params, titles=titles, 
+                        cmaps=cmaps, d=dims, axis_color=axis_color, canvas_color=canvas_color)
+
+
+    @param.depends('date', 'bins')#, 'time')
+    def view_sun(self, view_fn=plot_sun):
         '''
 
         '''
-        filename = c.TOPO_LIST[self.date]
-        params = {'R': self.resolution, 'S': self.sigma}
+        ts = TimeSeries(self.date)
+        self.df = ts.reassign_bins(self.bins, ts.df)
+        _dict = ts.generate_angle_dict(self.bins)
 
-        test = Topography(filename)
-        grids = test.return_grids(params['R'], params['S'])
-        return view_fn(fn=filename, grids=grids, bounds=c.BOUNDS, params=params,
+        return view_fn(df=self.df, angle_dict=_dict, time=5, bins=self.bins,
                         canvas_color=canvas_color, axis_color=axis_color)
 
-    
+    #@param.depends('date', 'resolution', 'sigma', 'bins', 'obscure', 'time')
+    def view_correction(self, view_fn=plot_grids):
+        '''
+        
+        For more information on these algorithms, see docstrings in source.
+        '''
+        slope_and_aspect_grids = self.grids[1:]
+        corr = Correction()
+        corr_array = corr.calculate_correction(
+            grids=slope_and_aspect_grids, 
+            df=self.df,
+            time=5
+        )
 
+        titles = generate_titles(fn=self.fname, params=self.params, _type='time')
+        cmaps = ['magma', 'binary']
+        dims = {'figsize':(8.25,5), 'dpi':80, 'wspace':0.1, 'hspace':0, 
+                'top':0.85, 'bottom':0.05, 'left':0.095, 'right':0.95}
 
-# append elements to template
+        # corr_horizon_array = corr.calculate_horizons
+        grids = [corr_array, self.grids[0]] 
+        return view_fn(grids=grids, params=self.params, titles=titles, 
+                        cmaps=cmaps, d=dims, axis_color=axis_color, canvas_color=canvas_color)
 
-'''
-react.sidebar.append(date)
-react.sidebar.append(resolution)
-react.sidebar.append(sigma)
-
-react.main[:4, :6] = grids
-'''
-
+# append elements to template & call servable
 interact = Interact()
 react.sidebar.append(interact.param)
-react.main[:4, :6] = interact.grids
+react.main[0:2, 0:6] = interact.view_terrain
+react.main[2:4, 0:2] = interact.view_sun
+react.main[2:4, 2:6] = interact.view_correction
 
 html_head = codecs.open("terrain-corrector/static/header.html", 'r')
 header = pn.pane.HTML(html_head.read())
 react.header.append(header)
 
-# define servable
 servable = react.servable()
--- terrain/plot.py
+++ terrain/plot.py
@@ -1,37 +1,63 @@
+'''
+This module defines static functions for plotting.
+'''
+
 import numpy as np
 import matplotlib.pyplot as plt
 import terrain.config as c
 
-def plot_grids(fn, grids, bounds, params, canvas_color, axis_color,
-                figsize=(12,5), dpi=80, wspace=0.05, hspace=0, 
-                left=0.05, right=0.97, top=0.79, bottom=0.1):
+
+def generate_titles(fn, params, _type):
     '''
 
     '''
-    fig, ax = plt.subplots(1,3, figsize=figsize, dpi=dpi)
+    assert (_type in ('topo', 'time'), 
+        f'_type must be one of \'topo\' or \'time\', got f{_type}')
 
-    #titles are added to the colorbars at the end of this function
     title_date = fn[:4] + '-' + fn[4:6] + '-' + fn[6:8]
     title_line_2 = f'\nR, S ={params["R"]}, {params["S"]}'
-    titles = [  
-        f'{title_date}: Elevation'+title_line_2,
-        f'{title_date}: Slope'+title_line_2,
-        f'{title_date}: Aspect (South=0, East +)'+title_line_2
-    ]
-    cmaps = ['viridis', 'YlOrBr', 'hsv']
-    ranges = [(np.min(grids[i]), np.max(grids[i])) for i in range(2)]
+
+    if _type == 'topo':
+        titles = [  
+            f'{title_date}: Elevation'+title_line_2,
+            f'{title_date}: Slope'+title_line_2,
+            f'{title_date}: Aspect (South=0, East +)'+title_line_2
+        ]
+    elif _type == 'time':
+        titles = [
+            f'{title_date}: Terrain Correction' + title_line_2,
+            f'{title_date}: Current Obsruction' + title_line_2
+        ]
+
+    return titles
+
+def plot_grids(grids, titles, params, cmaps, d, 
+                axis_color, canvas_color):
+    '''
+
+    '''
+    _len = len(grids)
+    iterables = [grids, titles, cmaps]
+    for i in range(len(iterables)):
+        arg = iterables[i]
+        assert len(arg) == _len, f'{arg} length not {_len}.'
+
+    fig, ax = plt.subplots(1, _len, figsize=d['figsize'])
+
+    ranges = [(np.min(grids[i]), np.max(grids[i])) for i in range(_len-1)]
     ranges.append((-180, 180))
+
     ticks = np.linspace(0, params['R']-1, 4)
-    
-    e_mn, e_mx, n_mn, n_mx, _, _ = bounds
-    xlabels = [str(e_mn + i)[-2:] for i in range(4)]
-    ylabels = [str(n_mn + i)[-2:] for i in range(4)]
+    xlabels = [str(c.EAST_MIN + i)[-2:] for i in range(4)]
+    ylabels = [str(c.NORTH_MIN + i)[-2:] for i in range(4)]
 
     ims = []
-    for i in range(3):
+    for i in range(_len):
         im = ax[i].imshow(
-                grids[i], cmap=cmaps[i], origin='lower',
-                vmin=ranges[i][0], vmax=ranges[i][1]
+                X = grids[i],
+                cmap = cmaps[i], 
+                vmin=ranges[i][0], vmax=ranges[i][1],
+                origin='lower'
             )
         ims.append(im)
         
@@ -45,18 +71,17 @@ def plot_grids(fn, grids, bounds, params, canvas_color, axis_color,
 
         if i == 0:
             ax[i].set_yticklabels(labels=ylabels)
-            ax[i].set_ylabel(f'Northing (+{str(n_mn)[:-2]}e2)')
+            ax[i].set_ylabel(f'Northing (+{str(c.NORTH_MIN)[:-2]}e2)')
         else:
             ax[i].set_yticklabels(labels=[])
             if i == 1:
-                ax[i].set_xlabel(f'Easting (+{str(e_mn)[:-2]}e2)')
+                ax[i].set_xlabel(f'Easting (+{str(c.EAST_MIN)[:-2]}e2)')
         ax[i].set_aspect("equal")
 
-    #adjust margins before adding colorbars
-    plt.subplots_adjust(left=left, right=right, top=top, bottom=bottom, wspace=wspace, hspace=hspace)
-
-    #add the colorbars
-    for i in range(3):
+    plt.subplots_adjust(left=d['left'], right=d['right'], top=d['top'], 
+                        bottom=d['bottom'], wspace=d['wspace'], hspace=d['hspace'])
+    
+    for i in range(_len):
         p = ax[i].get_position().get_points().flatten()
         ax_cbar = fig.add_axes([p[0], 0.85, p[2]-p[0], 0.05])
         ax_cbar.set_title(titles[i], loc='left', color=axis_color)
@@ -67,17 +92,56 @@ def plot_grids(fn, grids, bounds, params, canvas_color, axis_color,
             cb.set_ticks(cbar_ticks)
 
     fig.patch.set_facecolor(canvas_color)
-    plt.close()
+
     return fig
 
 
-#TEST AREA
-'''
-fn = c.TOPO_LIST[-6]
-params = {'R': 300, 'S':2.0}
+def plot_sun(df, angle_dict, time, bins, canvas_color, axis_color,
+            figsize=(4,5), top=1, bottom=0, left=0.2, right=0.99):
+    '''
 
-test = Topography(fn)
-grids = test.return_grids(params['R'], params['S'])
-simple_triptych(filename=fn, grids=grids, 
-                bounds=c.BOUNDS, params=params)
-'''
+    '''
+    fig = plt.figure(figsize=figsize)
+
+    tks = [np.deg2rad(a) for a in np.linspace(0,360,8,endpoint=False)]
+    xlbls = np.array(['N','45','E','135','S','225','W','315'])
+
+    ax = fig.add_subplot(111, projection='polar')
+    ax.set_theta_zero_location('N')
+    ax.set_xticks((tks))
+    ax.set_xticklabels(xlbls, rotation="vertical", size=12)
+    ax.tick_params(axis='x', pad = 0.5)
+    ax.set_theta_direction(-1)
+    ax.set_rmin(0)
+    ax.set_rmax(90)
+    ax.set_rlabel_position(90)
+    ax.set_facecolor(canvas_color)
+    ax.spines['polar'].set_color(axis_color)
+    ax.xaxis.label.set_color(axis_color)
+    ax.yaxis.label.set_color(axis_color)
+    ax.tick_params(axis='x', colors=axis_color)
+    ax.tick_params(axis='y', colors=axis_color)
+    
+    col = df['bin_assignment']
+    xs = [np.deg2rad(angle_dict[entry]) for entry in col]
+    x  = np.deg2rad(angle_dict[col.iloc[time]])
+    ys = df['solar_altitude']
+    y  = df['solar_altitude'].iloc[time]
+
+    ax.scatter(xs,ys, s=10, c='orange',alpha=0.5)
+    ax.scatter(x, y, s=500, c='gold',alpha=1)
+    
+    date = df['MeasDateTime'].iloc[time]
+    line1=f'{date} Sun Position'
+    line2=f'Azi, SZA={np.around((np.rad2deg(x),y),1)}, Bins={bins}'
+
+    plt.subplots_adjust(top=top, bottom=bottom, left=left, right=right)
+
+    p = ax.get_position().get_points().flatten()
+    ax_cbar = fig.add_axes([p[0]+0.085, 0.85, p[2]-p[0], 0.05])
+    ax_cbar.set_title(line1+'\n'+line2, loc='left', color=axis_color)
+    ax_cbar.axis('off')
+
+    fig.patch.set_facecolor(canvas_color)
+    return fig
+    
