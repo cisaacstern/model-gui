329a5b53597aea50eacde47ee7cd1e5d43748830
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-01-22 16:46:10 -0800

    minimal horizon functionality

--- _app.py
+++ _app.py
@@ -1,6 +1,7 @@
 '''
 
 '''
+import logging
 import codecs
 
 import param
@@ -12,6 +13,9 @@ from terrain.topo import Topography
 from terrain.time import TimeSeries
 from terrain.params import Correction, Horizon
 from terrain.plot import plot_grids, generate_titles, plot_sun
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 pn.extension()
 pn.config.sizing_mode = 'scale_both'
@@ -27,13 +31,24 @@ class Interact(param.Parameterized):
     #---parameters---#
     nums = list(c.DATE_DICT.values()) #update this
     date = param.Selector(default=70, objects=nums)
-    resolution = param.Integer(150, bounds=(10, 300), step=10)
+    resolution = param.Integer(30, bounds=(10, 300), step=10)
     sigma = param.Number(1.0, bounds=(0.0, 3.0))
     bins = param.Integer(32, bounds=(8,64), step=8)
+
+    prev_date = 100
+    prev_bin_count = 100
+    prev_bin = 100
     #---preview controls---#
     time = param.Integer(5, bounds=(0,100)) #needs regen each time 
 
-    def set_view_attributes(self):
+    prev_time = 5
+
+    #---------------------------------------------------------------------
+    # Terrain: Setter & Viewer
+    #---------------------------------------------------------------------
+
+    @param.depends('date', 'resolution', 'sigma')
+    def set_terrain(self):
         '''
         Instance method for resetting key attributes.
         Note that though this is called from within self.view_grids(),
@@ -41,8 +56,11 @@ class Interact(param.Parameterized):
         '''
         self.fname = c.TOPO_LIST[self.date]
         self.params = {'R':self.resolution, 'S':self.sigma, 'B':self.bins}
+        logger.debug('Params are %s ', self.params)
 
         test = Topography(self.fname)
+        logger.debug('Topography class was instantiated with fname %s ', self.fname)
+
         self.grids = test.return_grids(self.params['R'], self.params['S'])
 
     @param.depends('date', 'resolution', 'sigma')#, 'time')
@@ -51,7 +69,7 @@ class Interact(param.Parameterized):
         Two key functions here: (a) sets view attributes; and (b) serves
         the grid plot. dims are variablized because that depends.
         '''
-        self.set_view_attributes()
+
         titles = generate_titles(fn=self.fname, params=self.params, _type='topo')
         cmaps = ['viridis', 'YlOrBr', 'hsv']
         dims = {'figsize':(12,5), 'dpi':80, 'wspace':0.05, 'hspace':0, 
@@ -60,34 +78,104 @@ class Interact(param.Parameterized):
         return view_fn(grids=self.grids, params=self.params, titles=titles, 
                         cmaps=cmaps, d=dims, axis_color=axis_color, canvas_color=canvas_color)
 
+    #---------------------------------------------------------------------
+    # Sun: Setter & Viewer
+    #---------------------------------------------------------------------
+
+    @param.depends('date', 'bins')
+    def set_sun(self):
+        '''
+
+        '''
+        def _update_bins_and_dict(dataframe):
+            '''
+
+            '''
+            self.prev_bin_count = self.bins
+            self.df = TimeSeries.reassign_bins(self.bins, dataframe)
+            self._dict = TimeSeries.generate_angle_dict(self.bins)
+
+        logger.debug('prev_date and date are %s and %s', 
+                    self.prev_date, self.date)
+
+        logger.debug('prev_bin_count and bins are %s and %s', 
+                    self.prev_bin_count, self.bins)
+
+        if self.date == self.prev_date:
+            logger.debug('Previous and current dates equal. Base df unchanged.')
+            if self.prev_bin_count != self.bins:
+                logger.debug('Bins were changed. Reassigning bins in df.')
+                _update_bins_and_dict(dataframe=self.df)
+        else:
+            logger.debug('Date was changed. Re-initializing dataframe.')
+            self.prev_date = self.date
+            ts = TimeSeries(self.date)
+            _update_bins_and_dict(dataframe=ts.df)
+        
 
     @param.depends('date', 'bins', 'time')
     def view_sun(self, view_fn=plot_sun):
         '''
 
         '''
-        ts = TimeSeries(self.date)
-        self.df = ts.reassign_bins(self.bins, ts.df)
-        _dict = ts.generate_angle_dict(self.bins)
-
         dims = {'figsize':(4,5), 'top':1, 'bottom':0, 'left':0.2, 'right':0.95}
 
-        return view_fn(df=self.df, angle_dict=_dict, time=self.time, bins=self.bins,
+        return view_fn(df=self.df, angle_dict=self._dict, time=self.time, bins=self.bins,
                         canvas_color=canvas_color, axis_color=axis_color, d=dims)
 
+    #---------------------------------------------------------------------
+    # Correction: Setter & Viewer
+    #---------------------------------------------------------------------
+
     @param.depends('date', 'resolution', 'sigma', 'bins', 'time')#'obscure', 
-    def view_correction(self, view_fn=plot_grids):
+    def set_horizons(self):
         '''
-        
+
         For more information on these algorithms, see docstrings in source.
         '''
+        self.time_bin = self.df['bin_assignment'].iloc[self.time]
+
+        logger.debug('prev_bin and time_bin are %s and %s', 
+                    self.prev_bin, self.time_bin)
+
+        if self.time_bin == self.prev_bin:
+            logger.debug('Previous and current time_bin equal. No update made.')
+            pass
+        else:
+            self.prev_bin = self.time_bin
+            logger.debug('Update: time_bin was reset to %s ', self.time_bin)
+            self.azi = self._dict[self.time_bin]
+            elev_grid, _, _ = self.grids
+            horz = Horizon(df=self.df, azimuth=self.azi, elev_grid=elev_grid)
+            self.rot_elev = horz.rotated_elevation_grid
+            self.rot_slope = horz.rotated_slope_array
+
+    @param.depends('date', 'resolution', 'sigma', 'bins', 'time')#'obscure',
+    def set_correction(self):
+
         slope_and_aspect_grids = self.grids[1:]
-        corr = Correction()
-        corr_array = corr.calculate_correction(
+        self.corr_array = Correction.calculate_correction(
             grids=slope_and_aspect_grids, 
             df=self.df,
             time=self.time
         )
+
+        # start working area
+
+        alt = self.df['solar_altitude'].iloc[self.time]
+        self.mask = Horizon.calc_obstruction_for_altitude(
+            altitude=alt, azimuth=self.azi, 
+            rot_elev=self.rot_elev, rot_slope=self.rot_slope,
+            resolution=self.params['R']
+        )
+        # end working area
+
+            
+    @param.depends('date', 'resolution', 'sigma', 'bins', 'time')#'obscure', 
+    def view_correction(self, view_fn=plot_grids):
+        '''
+        
+        '''
 
         titles = generate_titles(fn=self.fname, params=self.params, _type='time')
         cmaps = ['magma', 'binary']
@@ -95,9 +183,13 @@ class Interact(param.Parameterized):
                 'top':0.85, 'bottom':0.05, 'left':0.095, 'right':0.95}
 
         # corr_horizon_array = corr.calculate_horizons
-        grids = [corr_array, self.grids[0]] 
+        grids = [self.corr_array, self.mask] 
         return view_fn(grids=grids, params=self.params, titles=titles, 
                         cmaps=cmaps, d=dims, axis_color=axis_color, canvas_color=canvas_color)
+
+    #---------------------------------------------------------------------
+    # Model runner
+    #---------------------------------------------------------------------
 
     def run_model(self):
         # can i call the same functions?
@@ -106,6 +198,11 @@ class Interact(param.Parameterized):
 # append elements to template & call servable
 interact = Interact()
 react.sidebar.append(interact.param)
+react.sidebar.append(interact.set_terrain)
+react.sidebar.append(interact.set_sun)
+react.sidebar.append(interact.set_horizons)
+react.sidebar.append(interact.set_correction)
+
 react.main[0:2, 0:6] = interact.view_terrain
 react.main[2:4, 0:2] = interact.view_sun
 react.main[2:4, 2:6] = interact.view_correction
--- terrain/params.py
+++ terrain/params.py
@@ -1,5 +1,9 @@
+import logging
 import numpy as np
 from scipy import ndimage
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
 
 class Correction():
     '''
@@ -48,14 +52,14 @@ class Horizon():
     initalize this class based on the azimuth: because azimuths are reused for multiple timepoints
     i.e. altitudes. Azimuths are also the most computationally expensive.
     '''
-    def __init__(self, angle_dict, _bin, elev_grid):
+    def __init__(self, df, azimuth, elev_grid):
         '''
 
         '''
-        #self.df = df
-        self.azimuth = angle_dict[_bin]
-        self.rotated_grid, self.slope_array = self.initialize_horizons(
-            angle_dict=angle_dict, _bin=_bin, elev_grid=elev_grid
+        self.df = df
+        self.azimuth = azimuth
+        self.rotated_elevation_grid, self.rotated_slope_array = (
+            self._initialize_azimuth(azimuth=azimuth, elev_grid=elev_grid)
         )
 
     #---------------------------------------------------------------------
@@ -63,187 +67,198 @@ class Horizon():
     #---------------------------------------------------------------------
 
     @staticmethod
-    def _initialize_horizons(azimuth, elev_grid):
+    def _initialize_azimuth(azimuth, elev_grid):
         '''
         This is an static method which serves as a helper for __init__.
         It is static bc somehow I think that'll make it easier to @jit it.
         '''
-        rotated_grid = _rotate2azimuth(df, angle_dict, time, elev_grid)
-        horizon_indices = _calc_horizon_indices(grid=rotated_grid)
-        slope_array = _calc_horizon_slope(rotated_grid, horz_arr=horizon_indices)
+        logger.debug('Hello you\'re in the initalizer')
 
-        return rotated_grid, slope_array
-    
-    @staticmethod
-    def _rotate2azimuth(azimuth, elev_grid):
-        '''
-        rotates an elevation raster so that North is re-referenced to be
-        facing the solar azimuth. See Dozier page XX fig XX.
-        '''
-        #col = df['bin_assignment']
-        #azimuth = angle_dict[col.iloc[time]]
-        #azimuth = angle_dict[_bin]
-            
-        rot_grid = ndimage.rotate(input=elev_grid, angle=(-azimuth),
-                                    reshape=True, order=0, 
-                                    mode='constant', cval=np.nan)
+        def _rotate2azimuth(azimuth, elev_grid):
+            '''
+            rotates a grid so that North is re-referenced to be
+            facing the solar azimuth. See Dozier page XX fig XX.
+            '''
+            logger.debug('Step 1: you\'re in the rotater')
 
-        return rot_grid
-
-    @staticmethod
-    def _slope(g, i, j, k):
-        '''
-        g is the elevation grid, i and j are row indicies, k is the column index
-        '''
-        if g[j, k] >= g[i, k]:
-            return (g[j, k] - g[i, k])/(j-i)
-        else:
-            return 0
-
-    @staticmethod  
-    def _calc_horizon_indices(grid):
-        '''
-        elevG is the blurred *AND ROTATED* elevation grid. horzPt[] is an empty
-        *2D array* to be filled with the horzPts for each cell in THE ROTATED elevation grid
-        the horzPt for a given elevG[i, k] is expressed as an index value of elevG[]
-        '''
-        horz_arr = np.zeros(grid.shape, dtype=int)
-        nhorz = horz_arr.shape[0]
-
-        for k in range(0, nhorz-1):
-            horz_array[nhorz-1, k] = nhorz - 1 #the first entry is its own horizon
-            
-            for i in range(nhorz-2, -1, -1): #loop from next-to-end backward to beginning
-                j = i + 1
-                horzj = horz_arr[j, k]
-                i_to_j = _slope(grid, i=i, j=j, k=k)
-                i_to_horzj = _slope(grid, i=i, j=horzj, k=k)
-
-                while i_to_j < i_to_horzj:
-                    j = horz_j
-                    horzj = horz_arr[j, k]
-                    slope_i_to_j = _slope(grid, i=i, j=j, k=k)
-                    slope_i_to_horz_j = _slope(grid, i=i, j=horzj, k=k)
-
-                if i_to_j > i_to_horzj:
-                    horz_arr[i, k] = j
-                elif i_to_j == 0:
-                    horz_arr[i, k] = i
-                else:
-                    horz_arr[i, k] = horzj
-
-        return horz_arr
+            return ndimage.rotate(input=elev_grid, angle=(-azimuth),
+                    reshape=True, order=0, mode='constant', cval=np.nan)
         
-    @staticmethod  
-    def _calc_horizon_slope(grid, horz_arr, scale=0.01):
-        '''
-        takes a rotated elevation grid and a corresponding horzPt array,
-        from self.fwdHorz2D, and calculates the slope from each point on
-        the elevation grid to its horizon point in the horzPt array.
-        returns the elevation grid and a 'slope to horizon (radians)' array
-        '''
-        shape = grid.shape
-        slope_arr = np.zeros(shape)
-
-        for k in range(0, shape[0]):
-            for i in range(0, shape[0]):
-                slope_arr[i,k] = np.arctan2(
-                                    x1 = grid[horz_arr[i,k], k] - grid[i,k],
-                                    x2 = ((scale * horz_arr[i,k]) - (scale * i))
-                                )
-        return slope_arr
-
-    @staticmethod
-    def _obscured_points(self, time):
-        '''
-        takes an elevation array, a 'slope to horizon (radians)' array,
-        and a solar altitude as inputs,
-        and returns a (TILTED) array of visible points
-        '''
-        #elevG, hSlope_rad = self.slope2horz()
-        #elevG, hSlope_rad = self.horizon_dispatch()
-        solar_altitude = self.df['solar_altitude'].iloc[time]
-        solar_altitude = np.deg2rad(90 - solar_altitude)
-        shape = hSlope_rad.shape
-        mask = np.zeros(shape)
-        for k in range(0, shape[0]):
-            for i in range(0, shape[0]):
-                if hSlope_rad[i,k] > solar_altitude:
-                    mask[i,k] = 1
-                elif hSlope_rad[i,k] < solar_altitude:
-                    mask[i,k] = 0
-                else:
-                    mask[i,k] = -1
-                
-        #tiltedElevG = elevG
-        #tiltedMask = mask
-        return mask
-    
-    @staticmethod
-    def _bbox(array):
-        '''
-
-        '''
-        rows = np.any(array, axis=1)
-        cols = np.any(array, axis=0)
-        rmin, rmax = np.where(rows)[0][[0, -1]]
-        cmin, cmax = np.where(cols)[0][[0, -1]]
-
-        return rmin, rmax, cmin, cmax
-
-    @staticmethod
-    def _rerotate_mask(rot_grid, rot_mask, azimuth):   
-        '''
-        rotate mask back to original position, set non-1s to zeros
-        '''
-        
-        #rotated_elevG, tiltedMask = self.invisiblePoints()
-        #dimLength = self.resolution
-        
-        mask = ndimage.rotate(rot_mask, angle=azimuth, reshape=True, 
-                                order=0, mode='constant', cval=np.nan)
-
-        mask[np.isnan(mask)] = 0
-        mask[mask == 0.5] = 0
-
-        #create ref from the rotated elevation grid, set nans to zeros, extract extents from ref
-        ref = ndimage.rotate(rot_grid, angle=azimuth, reshape=True, 
-                                order=0, mode='constant', cval=np.nan)
-        ref[np.isnan(ref)] = 0
-        rmin, rmax, cmin, cmax = _bbox(array=ref)
-
-        #clip extractMask using referenced extents, reset extractMask 0's to nan's
-        mask = mask[rmin:rmax, rmin:cmax]
-        mask[mask==0] = np.nan
-
-        return mask
-    
-    @staticmethod
-    def _square_mask(mask, resolution):
-        '''
-
-        '''
-        target_shape = (resolution, resolution)
-
-        if mask.shape == target_shape:
-            mask = mask
-        elif mask.shape != target_shape:
-            row_diff = target_shape[0] - mask.shape[0]
-            col_diff = target_shape[1] - mask.shape[1]
-            row_stacker = np.zeros((mask.shape[0]))
-            col_stacker = np.zeros((mask[1],1))
-
-            if mask.shape[0] == mask.shape[1]:
-                for i in range(1,row_diff+1):
-                    mask = np.append(mask, [row_stacker], axis=0)
-                for i in range(1,col_diff+1):
-                    mask = np.append(mask, col_stacker, axis=1)
+        def _slope(g, i, j, k):
+            '''
+            g is the elevation grid, i and j are row indicies, k is the column index
+            '''
+            if g[j, k] >= g[i, k]:
+                return (g[j, k] - g[i, k])/(j-i)
             else:
-                mask = np.zeros(target_shape)
-            
-        mask[mask==0] = np.nan
+                return 0
+        
+        def _calc_horizon_indices(grid):
+            '''
+            the horzPt for a given elevG[i, k] is expressed as an index value of elevG[]
+            '''
 
-        return mask
+            horz_arr = np.zeros(grid.shape, dtype=int)
+            nhorz = horz_arr.shape[0]
+
+            logger.debug(f'Step 2: you\'re in the indexer, nhorz = {nhorz}')
+
+            for k in range(0, nhorz-1):
+                                
+                horz_arr[nhorz-1, k] = nhorz - 1 #the first entry is its own horizon
+
+                for i in range(nhorz-2, -1, -1): #loop from next-to-end backward to beginning
+                    
+                    j = i + 1
+                    horzj = horz_arr[j, k]
+                    i_to_j = _slope(grid, i=i, j=j, k=k)
+                    i_to_horzj = _slope(grid, i=i, j=horzj, k=k)
+
+                    while i_to_j < i_to_horzj:
+                        j = horzj
+                        horzj = horz_arr[j, k]
+                        i_to_j = _slope(grid, i=i, j=j, k=k)
+                        i_to_horzj = _slope(grid, i=i, j=horzj, k=k)
+
+                    if i_to_j > i_to_horzj:
+                        horz_arr[i, k] = j
+                    elif i_to_j == 0:
+                        horz_arr[i, k] = i
+                    else:
+                        horz_arr[i, k] = horzj
+
+            return horz_arr
+
+        def _calc_horizon_slope(grid, horz_arr, scale=0.01):
+            '''
+            takes a rotated elevation grid and a corresponding horzPt array,
+            from self.fwdHorz2D, and calculates the slope from each point on
+            the elevation grid to its horizon point in the horzPt array.
+            returns the elevation grid and a 'slope to horizon (radians)' array
+            '''
+            logger.warning('Step 3: you\'re in the sloper. WHAT IS SCALE?')
+
+            shape = grid.shape
+            slope_arr = np.zeros(shape)
+
+            for k in range(0, shape[0]):
+                for i in range(0, shape[0]):
+                    slope_arr[i,k] = (
+                        np.arctan2(
+                            grid[horz_arr[i,k], k] - grid[i,k],
+                            ((scale * horz_arr[i,k]) - (scale * i))
+                        )
+                    )
+            return slope_arr
+        
+        rotated_grid = _rotate2azimuth(azimuth, elev_grid)
+        horizon_indices = _calc_horizon_indices(grid=rotated_grid)
+        rotated_slope_array = (
+            _calc_horizon_slope(rotated_grid, horz_arr=horizon_indices)
+        )
+
+        return rotated_grid, rotated_slope_array    
+
+    #---------------------------------------------------------------------
+    # Public API
+    #---------------------------------------------------------------------
+
+    @staticmethod
+    def calc_obstruction_for_altitude(altitude, azimuth, rot_elev, rot_slope, 
+                                        resolution):
+        '''
+
+        '''
+        def _calc_rotated_mask(alt, rot_slope):
+            '''
+            takes an elevation array, a 'slope to horizon (radians)' array,
+            and a solar altitude as inputs, returns a (TILTED) array of visible points
+            '''
+            #assert alt 
+            alt = np.deg2rad(90 - alt)
+            shape = rot_slope.shape
+            mask = np.zeros(shape)
+            for k in range(0, shape[0]):
+                for i in range(0, shape[0]):
+                    if rot_slope[i,k] > alt:
+                        mask[i,k] = 1
+                    elif rot_slope[i,k] < alt:
+                        mask[i,k] = 0
+                    else:
+                        mask[i,k] = -1
+                    
+            return mask
+    
+        def _bbox(array):
+            '''
+            A helper for the _rerotate_mask method.
+            '''
+            rows = np.any(array, axis=1)
+            cols = np.any(array, axis=0)
+            rmin, rmax = np.where(rows)[0][[0, -1]]
+            cmin, cmax = np.where(cols)[0][[0, -1]]
+
+            return rmin, rmax, cmin, cmax
+
+        def _rerotate_mask(rot_elev, rot_mask, azi):   
+            '''
+            rotate mask back to original position, set non-1s to zeros
+            '''
+            
+            #rotated_elevG, tiltedMask = self.invisiblePoints()
+            #dimLength = self.resolution
+            
+            mask = ndimage.rotate(rot_mask, angle=azi, reshape=True, 
+                                    order=0, mode='constant', cval=np.nan)
+
+            mask[np.isnan(mask)] = 0
+            mask[mask == 0.5] = 0
+
+            #create ref from the rotated elevation grid, set nans to zeros, extract extents from ref
+            ref = ndimage.rotate(rot_elev, angle=azi, reshape=True, 
+                                    order=0, mode='constant', cval=np.nan)
+            ref[np.isnan(ref)] = 0
+            rmin, rmax, cmin, cmax = _bbox(array=ref)
+
+            #clip extractMask using referenced extents, reset extractMask 0's to nan's
+            mask = mask[rmin:rmax, rmin:cmax]
+            mask[mask==0] = np.nan
+
+            return mask
+    
+        def _square_mask(mask, resolution):
+            '''
+
+            '''
+            target_shape = (resolution, resolution)
+
+            if mask.shape == target_shape:
+                mask = mask
+            elif mask.shape != target_shape:
+                row_diff = target_shape[0] - mask.shape[0]
+                col_diff = target_shape[1] - mask.shape[1]
+                row_stacker = np.zeros((mask.shape[0]))
+                col_stacker = np.zeros((target_shape[1],1))
+
+                if mask.shape[0] == mask.shape[1]:
+                    for i in range(1,row_diff+1):
+                        mask = np.append(mask, [row_stacker], axis=0)
+                    for i in range(1,col_diff+1):
+                        mask = np.append(mask, col_stacker, axis=1)
+                else:
+                    mask = np.zeros(target_shape)
+                
+            mask[mask==0] = np.nan
+
+            return mask
+        
+        rotated_mask = _calc_rotated_mask(alt=altitude, rot_slope=rot_slope)
+        rerotated_mask = _rerotate_mask(
+            rot_elev=rot_elev, rot_mask=rotated_mask, azi=azimuth
+        )
+        square_mask = _square_mask(mask=rerotated_mask, resolution=resolution)
+
+        return square_mask
 
     #---------------------------------------------------------------------
     # Public API
